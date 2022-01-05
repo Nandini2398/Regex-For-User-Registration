89f875d04d370509fe3071a02debb3f3c83e4df6
Nandini2398 <yarranandhini1416@gmail.com>
2022-01-05 08:08:37 +0530

    ADDED:first name validation

---
+++ src/com/bridgelabz/regex/UserDetails.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.regex;
+
+public class UserDetails {
+	private String firstName;
+	public UserDetails(String firstName) {
+		super();
+		this.firstName = firstName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+}
+
