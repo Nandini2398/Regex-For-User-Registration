dd5e1ece75dc642a3cd07827f24d1e324c3cd4e6
Nandini2398 <yarranandhini1416@gmail.com>
2022-01-05 16:38:16 +0530

    ADDED:first name validation

--- src/com/bridgelabz/regex/RegexMain.java
+++ src/com/bridgelabz/regex/RegexMain.java
@@ -2,12 +2,12 @@ package com.bridgelabz.regex;
 import java.util.Scanner;
 
 public class RegexMain {
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter First-Name:");
-			String firstName = sc.next();
-			UserDetails userdetails = new UserDetails(firstName);
-			UserRegistration registration = new UserRegistration();
-			registration.register(userdetails);
-		}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First-Name:");
+		String firstName = sc.next();
+		UserDetails userdetails = new UserDetails(firstName);
+		UserRegistration registration = new UserRegistration();
+		registration.register(userdetails);
 	}
+}
--- src/com/bridgelabz/regex/UserDetails.java
+++ src/com/bridgelabz/regex/UserDetails.java
@@ -13,4 +13,3 @@ public class UserDetails {
 		this.firstName = firstName;
 	}
 }
-
