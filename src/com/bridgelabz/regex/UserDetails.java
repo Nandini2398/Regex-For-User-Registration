4d01e09d38118cd4ade6d114d47b153037f38503
Nandini2398 <yarranandhini1416@gmail.com>
2022-01-05 16:45:09 +0530

    ADDED:validation of last name

--- src/com/bridgelabz/regex/UserDetails.java
+++ src/com/bridgelabz/regex/UserDetails.java
@@ -2,14 +2,22 @@ package com.bridgelabz.regex;
 
 public class UserDetails {
 	private String firstName;
-	public UserDetails(String firstName) {
+	private String lastName;
+	public UserDetails(String firstName,String lastName) {
 		super();
 		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 	public String getFirstName() {
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 }
